8176a3f6a27f927829ca9b48848f4d0dfe06ec67
macsbook <ilgizsaparbekov@gmail.com>
2022-10-23 15:32:49 +0600

    fjnjv

---
+++ app.js
@@ -0,0 +1,18 @@
+const obj2 = {
+    height:200,
+    width:300,
+    tittle:"My block"
+}
+function ball (a, b,) {
+    let res = a ** b
+    return console.log(res)
+}
+function link (c, d) {
+    let finish = c ** d
+    return console.log(finish)
+}
+ball(200, 2)
+link(300, 2)
+
+// console.log(obj2.height * obj2.height)
+// console.log(obj2.width * obj2.width)
